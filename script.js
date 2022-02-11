a3688dcb43e77f9becf60f7b896b8ee45e52628f
AigerimAkmatalieva <akmatalieva2108@gmail.com>
2022-02-11 14:54:09 +0600

    added events

--- script.js
+++ script.js
@@ -14,7 +14,29 @@ example2a.addEventListener('click', function(event) {
 });
 let example3a = document.getElementById('example3');
 example3a.addEventListener('click', function(event) {
-  window.location.href = "http://upwork.com"
 
   event.prevendDefault();
 });
+
+document.body.addEventListener('contextmenu', function(event) {
+  event.prevendDefault();
+});
+
+document.querySelector('div').addEventListener('dblclick', function(event) {
+  alert('Hello!');
+  //event.prevendDefault();
+});
+let example = document.createElement('section');
+example.style.width = "20px";
+example.style.height = "20px";
+example.style.backgroundColor = "black";
+example.style.position = "absolute";
+document.body.append(example);
+
+document.body.addEventListener('mousemove', function(event) {
+  example.style.top = event.clienty = 'px';
+example.style.left = event.clientX = 'px';
+});
+
+
+
