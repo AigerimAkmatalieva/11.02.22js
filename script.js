af5f580dbb023faade07c98f1650f2e537dec3e4
AigerimAkmatalieva <akmatalieva2108@gmail.com>
2022-02-11 14:23:28 +0600

    added events

---
+++ script.js
@@ -0,0 +1,17 @@
+let example1a = document.getElementById('example1');
+example1a.addEventListener('click', function(event) {
+
+  event.prevendDefault();
+  //console.log(event);
+  //alert("Hello world");
+});
+let example2a = document.getElementById('example2');
+example2a.addEventListener('click', function(event) {
+
+  event.prevendDefault();
+});
+let example3a = document.getElementById('example3');
+example3a.addEventListener('click', function(event) {
+
+  event.prevendDefault();
+});
