b4341be3871e8241529177041d7d31132fa40e71
AigerimAkmatalieva <akmatalieva2108@gmail.com>
2022-02-11 14:27:14 +0600

    added events

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let example1a = document.getElementById('example1');
 example1a.addEventListener('click', function(event) {
+window.location.href = "http://bing.com"
 
   event.prevendDefault();
   //console.log(event);
@@ -7,11 +8,13 @@ example1a.addEventListener('click', function(event) {
 });
 let example2a = document.getElementById('example2');
 example2a.addEventListener('click', function(event) {
+  window.location.href = "http://linkedin.com"
 
   event.prevendDefault();
 });
 let example3a = document.getElementById('example3');
 example3a.addEventListener('click', function(event) {
+  window.location.href = "http://upwork.com"
 
   event.prevendDefault();
 });
